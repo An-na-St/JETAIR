4976dbeda4c9e374cf808e24ed9c271495a99754
An-na-St <an.na.st@mail.ru>
2022-06-04 23:20:41 +0300

    Add slider to info block

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,17 @@ $(document).ready(function() {
     $('.burger, .navigation').toggleClass('active')
   });
 });
+
+new Swiper('.service_options_container', {
+
+  autoHeight: false,
+
+  slidesPerView: 1,
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  loop: true,
+
+});
